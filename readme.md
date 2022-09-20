944dd870cd0869db20cab028c515037021a4e7ea
PiotrPress <admin@piotr.press>
2022-09-20 15:29:28 +0200

    Init

---
+++ readme.md
@@ -0,0 +1,34 @@
+# Configer
+
+This library loads/saves php array from/to a configuration file.
+
+## Installation
+
+```shell
+$ composer require piotrpress/configer
+```
+
+## Usage
+
+```php
+require __DIR__ . '/vendor/autoload.php';
+
+use PiotrPress\Configer;
+
+$config = new Configer( '.config', [
+    'key' => 'value'
+] );
+
+echo $config[ 'key' ]; 
+$config[ 'key' ] = 'new_value'
+
+$config->save();
+```
+
+## Requirements
+
+- PHP >= `7.4` version.
+
+## License
+
+[MIT](license.txt)
---
+++ src/Configer.php
@@ -0,0 +1,53 @@
+<?php declare( strict_types = 1 );
+
+namespace PiotrPress;
+
+class Configer implements \IteratorAggregate, \ArrayAccess, \Countable {
+    protected string $file = '';
+    protected array $data = [];
+
+    public function __construct( string $file, array $default = [], array $context = [] ) {
+        $this->file = $file;
+
+        if ( $context ) \extract( $context, \EXTR_SKIP );
+
+        $data = \is_file( $this->file ) ? require( $this->file ) : [];
+        $this->data = \is_array( $data ) ? \array_merge( $default, $data ) : [];
+    }
+
+    public function save() : bool {
+        return (bool)\file_put_contents(
+            $this->file,
+            \sprintf( '<?php return %s;', \var_export( $this->data, true ) )
+        );
+    }
+
+    public function toArray() : array {
+        return $this->data;
+    }
+
+    public function offsetExists( $offset ) : bool {
+        return isset( $this->data[ $offset ] );
+    }
+
+    public function offsetGet( $offset ) {
+        return $this->data[ $offset ] ?? null;
+    }
+
+    public function offsetSet( $offset, $value ) : void {
+        if ( \is_null( $offset ) ) $this->data[] = $value;
+        else $this->data[ $offset ] = $value;
+    }
+
+    public function offsetUnset( $offset ) : void {
+        \unset( $this->data[ $offset ] );
+    }
+
+    public function getIterator() : \Traversable {
+        return new \ArrayIterator( $this->data );
+    }
+
+    public function count() : int {
+        return \count( $this->data );
+    }
+}
